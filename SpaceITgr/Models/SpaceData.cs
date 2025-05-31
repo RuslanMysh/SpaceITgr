dfd89edfee83ad6b6e1ceace79590ea6150313ef
Ruslan <ruslanmyskovskij@gmail.com>
2025-05-31 23:15:00 +0700

    Коммит 31.05.25

--- SpaceITgr/Models/SpaceData.cs
+++ SpaceITgr/Models/SpaceData.cs
@@ -6,15 +6,15 @@ namespace SpaceITgr.Models
 	{
 		public static Dictionary<int, Planet> Planets = new Dictionary<int, Planet>()
 		{
-			{173, new Planet(9000000000, "Planet173", "Andromeda", true, PlanetType.Earth_Type, "Земля", DateTime.Now)},
-			{1731, new Planet(100000, "Satellite1731", "Andromeda", true, PlanetType.Satellite, "Луна", DateTime.Now)},
+			{173, new Planet(9000000000, "Planet173", "Andromeda", true, PlanetType.Earth_Type, "Земля", new DateTime(2199, 06, 8, 11, 30, 00))},
+			{1731, new Planet(100000, "Satellite1731", "Andromeda", true, PlanetType.Satellite, "Луна", new DateTime(2199, 06, 8, 12, 00, 00))},
 			{175, new Planet(340000000, "Planet175", "Andromeda", false, PlanetType.Earth_Type, "Марс", DateTime.Now)},
 		};
 
 		public static Dictionary<string, Quest> Quests = new Dictionary<string, Quest>()
 		{
-			{"p1", new Quest("Первый квест", "Блаблаблабла", new DateTime(2025, 05, 31, 11, 30, 00), QuestType.Current) },
-			{"s1", new Quest("Помощь учёному", "Блаблаблабла", new DateTime(2025, 06, 01, 14, 25, 00), QuestType.Current) }
+			{"p1", new Quest("Первый квест", "Блаблаблабла", new DateTime(2199, 05, 31, 11, 30, 00), QuestType.Current) },
+			{"s1", new Quest("Помощь учёному", "Блаблаблабла", new DateTime(2199, 06, 01, 14, 25, 00), QuestType.Current) }
 		};
 
 		public static List<Item> Inventory = new List<Item>()
