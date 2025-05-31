fb12d960d7e0cd47cb7d2f816b311487b802ff1b
Ruslan <ruslanmyskovskij@gmail.com>
2025-05-31 10:23:42 +0700

    Переработан _Layout

--- SpaceITgr/Models/SpaceData.cs
+++ SpaceITgr/Models/SpaceData.cs
@@ -8,7 +8,7 @@ namespace SpaceITgr.Models
 		{
 			{173, new Planet(9000000000, "Planet173", "Andromeda", true, PlanetType.Earth_Type, "Earth", DateTime.Now)},
 			{1731, new Planet(100000, "Satellite1731", "Andromeda", true, PlanetType.Satellite, "Moon", DateTime.Now)},
-			{175, new Planet(340000000, "Planet175", "Andromeda", true, PlanetType.Earth_Type, "Mars", DateTime.Now)},
+			{175, new Planet(340000000, "Planet175", "Andromeda", false, PlanetType.Earth_Type, "Mars", DateTime.Now)},
 		};
 	}
 }
