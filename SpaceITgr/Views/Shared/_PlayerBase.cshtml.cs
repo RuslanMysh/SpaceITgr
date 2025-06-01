2da1021b7e012f861b6e33efb0ad192e19178e55
Ruslan <ruslanmyskovskij@gmail.com>
2025-06-01 13:06:34 +0700

    Добавлены города, как точки

---
+++ SpaceITgr/Views/Shared/_PlayerBase.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpaceITgr.Views.Shared
+{
+    public class PlayerButtonsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
